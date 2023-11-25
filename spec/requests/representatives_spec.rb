f5fff5fcfa59cf1cb1cc1a724fd502e9eb3089e8
Pooja Patel <pnpatel@berkeley.edu>
2023-11-25 02:05:39 +0000

    reached 42% coverage

--- spec/requests/representatives_spec.rb
+++ spec/requests/representatives_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# spec/requests/representatives_spec.rb
-
 require 'rails_helper'
 require 'spec_helper'
 require 'google/apis/civicinfo_v2'
@@ -14,7 +12,7 @@ describe 'Representatives', type: :request do
       service.key = Rails.application.credentials[:GOOGLE_API_KEY]
       rep_info = service.representative_info_by_address(address: address)
 
-      # Create a Representative instance for testing
+      # Representative instance for testing
       @representative = Representative.create!(name: rep_info.officials.first.name, title: rep_info.offices.first.name)
     end
 
@@ -24,7 +22,6 @@ describe 'Representatives', type: :request do
       expect(response).to have_http_status(:success)
       expect(response.body).to include(@representative.name)
       expect(response.body).to include(@representative.title)
-      # Additional checks for other fields can be added here
     end
   end
 end
