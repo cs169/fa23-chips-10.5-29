26b1db8ec962c7167f8b89a4f8a271bdc7e8b5cb
Gautam Anand <gautamanand@berkeley.edu>
2023-11-25 08:09:15 +0000

    Task 1.3 one more

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,18 +6,14 @@ class SearchController < ApplicationController
   def search
     found_state = flash[:state_identifier] && flash[:fips_identifier]
 
-    if found_state
-      state_key = flash[:state_identifier]
-      fips_key = flash[:fips_identifier]
-
-      located_state = State.where(symbol: state_key).first
-      located_county = County.where(state_id: located_state.try(:id), fips_code: fips_key).first if located_state
-      @resolved_state = located_state
-      @resolved_county = located_county
+    if flash[:region] && flash[:area_code]
+      selected_state = State.find_by(symbol: flash[:region])
+      targeted_county = selected_state ? County.find_by(state_id: selected_state.id, fips_code: flash[:area_code]) : nil
     else
-      @resolved_state = nil
-      @resolved_county = nil
+      selected_state = nil
+      targeted_county = nil
     end
+    
 
     address = params[:address]
     service = Google::Apis::CivicinfoV2::CivicInfoService.new
