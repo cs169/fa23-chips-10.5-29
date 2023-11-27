6a35d4cafcedfe8f6b9ef518132cefc6434a80ed
Gautam Anand <gautamanand@berkeley.edu>
2023-11-27 13:29:02 +0000

    changed code to resolve

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,15 +6,14 @@ class SearchController < ApplicationController
   def search
     if flash[:state] && flash[:county]
       @state = State.find_by(symbol: flash[:state])
-      @county = County.find_by(state_id: @state.id, fips_code: flash[:county]) if @state
-    end
-    if flash[:state_identifier] && flash[:fips_identifier]
-      located_state = State.find_by(symbol: flash[:state_identifier])
-      @resolved_state = located_state
-      @resolved_county = located_state ? County.find_by(state_id: located_state.id, fips_code: flash[:fips_identifier]) : nil
+      @county = County.find_by(
+        state:     @state.id,
+        fips_code: flash[:county]
+      )
     else
-      @resolved_state = @resolved_county = nil
-    end    
+      @state = nil
+      @county = nil
+    end
 
     address = params[:address]
     service = Google::Apis::CivicinfoV2::CivicInfoService.new
