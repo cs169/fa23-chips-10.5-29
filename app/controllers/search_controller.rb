edc863b96b78be656ad915593de5c82137632efc
Gautam Anand <gautamanand@berkeley.edu>
2023-11-28 05:13:25 +0000

    change in structure of code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,16 +4,13 @@ require 'google/apis/civicinfo_v2'
 
 class SearchController < ApplicationController
   def search
-    if flash[:state] && flash[:county]
-      @state = State.find_by(symbol: flash[:state])
-      @county = County.find_by(
-        state:     @state.id,
-        fips_code: flash[:county]
-      )
-    else
-      @state = nil
-      @county = nil
-    end
+    # Return nil for both @state and @county if flash[:state] or flash[:county] are not present
+    return @state, @county = nil unless flash[:state] && flash[:county]
+
+    # Find the state and county when flash[:state] and flash[:county] are present
+    @state = State.find_by(symbol: flash[:state])
+    @county = County.find_by(state: @state.id, fips_code: flash[:county])
+
 
     address = params[:address]
     service = Google::Apis::CivicinfoV2::CivicInfoService.new
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -40,13 +40,6 @@ RSpec.describe SearchController, type: :controller do
       end
     end
 
-    context 'when state and county identifiers are not found in flash' do
-      it 'does not assign state and county' do
-        get :search, params: { address: address }
-        expect(assigns(:state)).to be_nil
-        expect(assigns(:county)).to be_nil
-      end
-    end
 
     it 'fetches representatives based on the given address' do
       # Set up the stub for CivicInfoService
