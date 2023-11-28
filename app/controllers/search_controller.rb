78339bd75218d971bbf7e9bfc99ae6368c43241f
Gautam Anand <gautamanand@berkeley.edu>
2023-11-28 05:40:07 +0000

    committting to resolve

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,14 +4,14 @@ require 'google/apis/civicinfo_v2'
 
 class SearchController < ApplicationController
   def search
-    @selected_state, @selected_county = if flash[:state] && flash[:county]
-                                          [State.find_by(symbol: flash[:state]),
-                                           County.find_by(state:     State.find_by(symbol: flash[:state]).id,
-                                                          fips_code: flash[:county])]
-                                        else
-                                          [nil, nil]
-                                        end
-
+    if flash[:state] && flash[:county]
+      @selected_state = State.find_by(symbol: flash[:state])
+      @selected_county = County.find_by(state: @selected_state.id, fips_code: flash[:county])
+    else
+      @selected_state = nil
+      @selected_county = nil
+    end
+    
     address = params[:address]
     service = Google::Apis::CivicinfoV2::CivicInfoService.new
     service.key = Rails.application.credentials[:GOOGLE_API_KEY]
