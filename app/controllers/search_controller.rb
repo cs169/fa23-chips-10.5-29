0003c288a76e90e39cae777ccd2bf4fb5edea61c
Gautam Anand <gautamanand@berkeley.edu>
2023-11-28 05:26:16 +0000

    comitting

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,12 +4,9 @@ require 'google/apis/civicinfo_v2'
 
 class SearchController < ApplicationController
   def search
-    # Return nil for both @state and @county if flash[:state] or flash[:county] are not present
-    return @state, @county = nil unless flash[:state] && flash[:county]
-
-    # Find the state and county when flash[:state] and flash[:county] are present
-    @state = State.find_by(symbol: flash[:state])
-    @county = County.find_by(state: @state.id, fips_code: flash[:county])
+    @selected_state, @selected_county = flash[:state] && flash[:county] ? 
+  [State.find_by(symbol: flash[:state]), County.find_by(state: State.find_by(symbol: flash[:state]).id, fips_code: flash[:county])] : 
+  [nil, nil]
 
     address = params[:address]
     service = Google::Apis::CivicinfoV2::CivicInfoService.new
